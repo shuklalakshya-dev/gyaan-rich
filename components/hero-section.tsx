fcd3afe7874e23dad855c965ef44ebc32ea68ede
shuklalakshya-dev <lakshyashukla31@gmail.com>
2025-10-28 22:12:31 +0530

    ./

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -21,6 +21,7 @@ export function HeroSection() {
         />
       </div>
       
+      
       {/* Decorative elements */}
       <div className="absolute top-0 right-0 w-96 h-96 bg-accent/10 rounded-full blur-3xl"></div>
       <div className="absolute bottom-0 left-0 w-96 h-96 bg-accent/5 rounded-full blur-3xl"></div>
