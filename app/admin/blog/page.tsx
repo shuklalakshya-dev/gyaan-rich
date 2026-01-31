322340734bf4c06f2be9c9898cf47f2761ff2f90
Lakshya Shukla <lakshyashukla31@gmail.com>
2026-01-31 10:27:55 +0530

    Fix author name typo in form data

--- app/admin/blog/page.tsx
+++ app/admin/blog/page.tsx
@@ -191,7 +191,7 @@ export default function BlogPage() {
             title: "",
             excerpt: "",
             content: "",
-            author: "Gyaan Rich",
+            author: "GyanRich",
             category: "Education",
             image: "",
           })
